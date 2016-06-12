f73bc8bc4abd6eb054f758d722cf3a15c76261ea
otake <otake.hiroyo@gmail.com>
2016-06-12 07:13:39 +0000

    4th commit

--- password_change.php
+++ password_change.php
@@ -11,79 +11,61 @@ session_start();
 
 /*SESSIONの取り出し*/
 $sales_id = $_SESSION['sales_id'];
-//var_dump($_SESSION);
 
 /*POSTの受け取り*/
 $c_pass = @$_POST['c_pass'];
 $n_pass = @$_POST['n_pass'];
 $n_pass_r = @$_POST['n_pass_r'];
-//var_dump($c_pass);
-//var_dump($n_pass);
-//var_dump($n_pass_r);
 
-/* lu_salesからログイン中のユーザのデータを読みだす。wehre=$_SESSION['sales_id']*/
+/* lu_salesからログイン中のユーザのデータを読みだす。*/
 $con = pg_connect("host=127.0.0.1 port=5432 dbname=download user=postgres password=");
 
 if ($con == false) {
-//	print("認証データベースへの接続に失敗しました。理由；$php_errormsg<br>\n");
 	print("認証データベースへの接続に失敗しました。");
 	exit;
 }
 
 // テキストデータをエスケープする
 $escaped = pg_escape_string($sales_id);
+
 $sql = "SELECT * FROM lu_sales WHERE sales_id='$escaped'";
 $result = pg_query($con,$sql);
 $sales_password = pg_fetch_result($result,'password');
-//var_dump($sales_password);
-//exit;
 
-/*画面で入力されたパスワードのバリデーション(データチェックすること)*/
+/*画面で入力されたパスワードのバリデーション*/
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$errors = array();
 
-	/*現在のパスワード(画面で入力された)DBに入っている現在のパスワードが一致してるかチェック
-	一致してなければエラー
-	一致してれば次のチェック*/
 	if ($sales_password !== $c_pass) {
-		$smarty->assign("c_pass_error", "*現在のパスワードがまちがっています。");
-		$smarty->display($p_c_tpl);
-		exit;
+	  $errors['c_pass_error'] = "*現在のパスワードが間違っています。<br>";
+	}
+
+	if ($n_pass !== $n_pass_r || $n_pass == "") {
+	  $errors['n_pass_error'] = "*新しいパスワードは同じものを2回入力して下さい。<br>";
+	}
+
+	if (strlen($n_pass) < 8) {
+	  $errors['count_error'] = "*8文字以上のパスワード変更を入力して下さい。<br>";
 	} else {
-		//echo "c_pass OK";
+		$escaped2 = pg_escape_string($n_pass);
+		$sql = "UPDATE lu_sales SET password='$escaped2' WHERE sales_id='$escaped'";
+		$result = pg_query($con,$sql);
+        } 
 
-		if ($n_pass !== $n_pass_r || $n_pass == "") {
-			$smarty->assign("n_pass_error", "*新しいパスワードがまちがっています。");
-			$smarty->display($p_c_tpl);
-			exit;
-		} else {
-			//echo "n_pass OK";
+	if ($result == false) {
+	  $errors['sql_error'] = "*クエリが正しく動作していません。";
+	}
 
-			if (strlen($n_pass) < 8) {
-				//$c = strlen($n_pass);	
-                                //var_dump($c);
-                                //exit;
-				$smarty->assign("count_error", "*パスワードは8文字以上を入力して下さい。");
-				$smarty->display($p_c_tpl);
-				exit;
-			} else {
+	if ($errors) {
+	  $smarty->assign('errors', $errors);
+	  $smarty->display($p_c_tpl);
+	  exit;
+	}
 
-				$escaped2 = pg_escape_string($n_pass);
-				$sql = "UPDATE lu_sales SET password='$escaped2' WHERE sales_id='$escaped'";
-				$result = pg_query($con,$sql);
+header('Location: password_change_thanks.php');
 
-				if ($result == false) {
-					$smarty->assign("sql_error", "*クエリが正しく動作していません。");
-					$smarty->display($p_c_tpl);
-					exit;
-				} else {
-					header("Location:password_change_thanks.php");
-			}
-		} 
 
-	} 
-}
-
-} else {
+} else { 
 
 /*Smartyでhtmlを読み込む*/
 $smarty -> display($p_c_tpl);
